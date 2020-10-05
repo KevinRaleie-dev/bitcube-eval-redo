15100ea0211d4c7698ba945f7fdb89756493bd27
KevinRaleie-dev <kkevraleie@gmail.com>
2020-10-05 22:01:42 +0200

    Added auth context for user login

--- BitcubeClient/client/src/App.js
+++ BitcubeClient/client/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import LandingPage from './pages/LandingPage';
@@ -8,12 +7,22 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
+import AuthContext from './context/AuthContext';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  const value = useMemo(() => ({
+    user, 
+    setUser
+  }), [user, setUser])
+
   return (
   <ThemeProvider>
     <CSSReset />
     <Router>
+      <AuthContext.Provider value={value}>
       <Navbar />
       <Switch>
         <Route path='/' component={LandingPage} exact />
@@ -22,6 +31,7 @@ function App() {
         <Route path='/login' component={Login} exact />
         <Route path='/profile' component={Profile} exact />
       </Switch>
+      </AuthContext.Provider>
     </Router>
   </ThemeProvider>
   );
--- BitcubeClient/client/src/pages/Login.jsx
+++ BitcubeClient/client/src/pages/Login.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useContext } from 'react';
+import Container from '../components/Container';
+import { useForm } from 'react-hook-form';
+import { FormControl, Stack, FormLabel, Input, Button, Text, Checkbox } from '@chakra-ui/core';
+import AuthContext from '../context/AuthContext';
+import axios from 'axios';
 
 const Login = () => {
+    const { user, setUser } = useContext(AuthContext);
+    const {register, handleSubmit, formState} = useForm();
+
+    // TODO: a few things we need to do here...
+    // when everything checks out when logging in a user
+    // we store the user response from the server inside the app's context
+    // eg setUser(response)
+    // const onSubmit = () => {
+    //     // console.log(data);
+    // };
+
+    const onSubmit = async (data) => {
+        // const {email, password} = data;
+
+        const BASE_URL = 'https://localhost:5001/api/users/login';
+
+        try {
+           const response = await axios.post(BASE_URL, data);
+
+        console.log(response);
+        setUser(response);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <div>
-            <Navbar />
-        </div>
+        
+            <Container>
+                <Text fontSize='5xl' fontWeight='bold' marginBottom={4}>Login</Text>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Stack spacing={3}>
+                    <FormControl isRequired>
+                        <FormLabel htmlFor='email'>Email</FormLabel>
+                        <Input name='email' ref={register} type='email' id='email' placeholder=''/>
+                    </FormControl>
+                    <FormControl isRequired>
+                        <FormLabel htmlFor='password'>Password</FormLabel>
+                        <Input name='password' ref={register} type='password' id='password' placeholder=''/>
+                    </FormControl>
+                    {/* <Checkbox value="Remember">Remember me</Checkbox>   */}
+                    <Button
+                    type='submit' 
+                    isLoading={formState.isSubmitting}
+                    loadingText='Logging in...'
+                    >Login</Button>
+                    </Stack>
+                </form>
+                <pre>{JSON.stringify(user, null, 2)}</pre>
+            </Container>
     )
 }
 
