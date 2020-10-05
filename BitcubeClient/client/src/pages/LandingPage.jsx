e0800ff235abe7d1f88eba224982b0f5ddf3ae66
KevinRaleie-dev <kkevraleie@gmail.com>
2020-10-05 13:40:01 +0200

    Designed a basic landing page

--- BitcubeClient/client/src/pages/LandingPage.jsx
+++ BitcubeClient/client/src/pages/LandingPage.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-// import { Button } from '@chakra-ui/core';
-import Navbar from '../components/Navbar';
+import { Box, Button, Grid, Text } from '@chakra-ui/core';
+import { Link } from 'react-router-dom'
+import Social from '../images/social.svg';
 
 const LandingPage = () => {
     return (
-        <div>
-            <Navbar />
-        </div>
+        <Grid templateColumns="repeat(2, 1fr)" gap={3} padding={10}>
+            <Box w="100%" h="70vh" marginTop={20}>
+                <Text fontSize='6xl' fontWeight='bold'>
+                    The Platform For Building Friendships
+                </Text>
+                <Text fontSize='xl'>
+                    Share awesome content with people you meet on the platform. Write posts about your day, week or
+                    just about anything that floats your boat. Friend.ly is a platform for friends so anything you put
+                    on here will only be seen by people in your friendlist. Sign up and start making some friends!
+                </Text>
+                <Link to='/register'>
+                    <Button variantColor='blue' marginTop={5} bg='blue.300' color='white'>
+                        Get Started
+                    </Button>
+                </Link>
+            </Box>
+            <Box w="100%" h="70vh" justifyContent="center" alignItems="center" display="flex">
+                <img src={Social} alt="social image"/>
+            </Box>
+        </Grid>
     )
 }
 
