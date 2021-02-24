e43d0ec67bca641e8defcfb863b680af5d58dbaf
KevinRaleie-dev <kkevraleie@gmail.com>
2021-02-24 16:43:01 +0200

    added `marginTop` as a prop

--- BitcubeClient/client/src/components/Container.jsx
+++ BitcubeClient/client/src/components/Container.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Box } from '@chakra-ui/core';
 
 const Container = (props) => {
-    const { children } = props;
+    const { children, marginTop } = props;
     return (
        <Box
-       mt={8}
-       maxWidth='800px'
+       mt={marginTop}
+       maxWidth='600px'
        mx='auto'
        width='100%'
        >
