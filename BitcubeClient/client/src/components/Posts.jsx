fb3d428cf0f4cebd9338c303e81a6d895be5ff77
KevinRaleie-dev <kkevraleie@gmail.com>
2021-03-04 17:20:05 +0200

    Refactor: cleaned up posts component

--- BitcubeClient/client/src/components/Posts.jsx
+++ BitcubeClient/client/src/components/Posts.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import List from './List';
 import { useQuery } from 'react-query';
 import {fetchPosts} from '../utils/api';
+import { Box, Spinner } from '@chakra-ui/core';
 
-const Posts = ({firstName}) => {
+const Posts = () => {
     const {data, isLoading, isError, error} = useQuery('posts', fetchPosts);
-
     
-    if (isLoading){
-        return <span>loading...</span>
-    }
-    
-    if(isError) {
-        return <span>Error: {error.message}</span>
-    }
-
-    const sortedData = [...data.data];
-
     return (
-        <>
+        <React.Fragment>
             {
-                sortedData.reverse().map((post) => (
-                        <List key={post.Id} post={post.Content} />
-                    // <li key={post.Id}>{post.Content}</li>
-                ))
+                isLoading ? <>
+                    <Box
+                    display='flex'
+                    justifyContent='center'
+                    mt={3}
+                    >
+                        <Spinner />
+                    </Box>
+                </> : <>
+                
+                    {
+                        [...data.data].reverse().map((post) => (
+                            <List key={post.Id} post={post.Content} />
+                        ))
+                    }
+                </>
             }
-        </>
+            {
+                isError && <span>Something went wrong: {error.message}</span>
+            }
+        </React.Fragment>
     )
 }
 
